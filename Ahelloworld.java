69e76a0e29ff50b529f3693af2e34cac51df76ef
miche11e-y <115690792+miche11e-y@users.noreply.github.com>
2023-09-01 16:13:42 -0700

    Testing

---
+++ Ahelloworld.java
@@ -0,0 +1,14 @@
+public class Ahelloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("anything idk");
+        char time = 'a';
+        int age = 15;
+        System.out.println("I am " + age + " years old");
+        // int char string boolean float double
+        // type name = value;
+        // int time = 12;
+        // int, char, float, double, boolean, string
+        // + - * /
+    }
+}
